a8d238d79284dd7828b6cc422762db256f7fbeca
hilianawendy-lgtm <hilianawendy@gmail.com>
2025-12-01 10:51:39 +0100

    Test Gitleaks faux secret

--- src/main/java/com/example/democyber/HelloController.java
+++ src/main/java/com/example/democyber/HelloController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    // Faux secret pour tester Gitleaks (TP3)
+    String testSecret = "ghp_FAKESECRET1234567890000000000000000";
+
     @GetMapping("/")
     public String home() {
         return "Bonjour depuis democyber !";
